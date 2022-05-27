409b5a71ec44b537bcf4cce050925bea7c16b439
Gustavo <gustavoquintero9105@gmail.com>
2022-05-27 11:17:02 -0300

    -

--- projeto/main.py
+++ projeto/main.py
@@ -1,28 +1,31 @@
+import numpy as np
 import matplotlib.pyplot as plt
+# import pandas as pd
+import mnist_reader
 
-from tensorflow import keras
+# from tensorflow import keras
 
-fashion_mnist = keras.datasets.fashion_mnist
-(trImages, trLabels), (tImages, tLabels) = fashion_mnist.load_data()
+# fashion_mnist = keras.datasets.fashion_mnist
+# (trImages, trLabels), (tImages, tLabels) = fashion_mnist.load_data()
 
-# print(np.shape(trImages))
-# print(np.shape(tImages))
-# print(np.max(trImages[0]))
-# print(np.min(trImages[0]))
+# # print(np.shape(trImages))
+# # print(np.shape(tImages))
+# # print(np.max(trImages[0]))
+# # print(np.min(trImages[0]))
 
-lookup = {
-    0: 'T-shirt',
-    1: 'Trouser',
-    2: 'Pullover',
-    3: 'Dress',
-    4: 'Coat',
-    5: 'Sandal',
-    6: 'Shirt',
-    7: 'Sneaker',
-    8: 'Bag',
-    9: 'Ankle boot'
-}
+# lookup = {
+#     0: 'T-shirt',
+#     1: 'Trouser',
+#     2: 'Pullover',
+#     3: 'Dress',
+#     4: 'Coat',
+#     5: 'Sandal',
+#     6: 'Shirt',
+#     7: 'Sneaker',
+#     8: 'Bag',
+#     9: 'Ankle boot'
+# }
 
-print(trLabels[10])
-plt.imshow(trImages[10],cmap='gray',vmin=0,vmax=255)
-plt.show()
+# print(trLabels[10])
+# plt.imshow(trImages[10],cmap='gray',vmin=0,vmax=255)
+# plt.show()
